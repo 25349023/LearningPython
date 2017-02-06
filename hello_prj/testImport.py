ca88a008d416539cfdfe4d6d944aa21468057d08
25349023 <25349023.qq@gmail.com>
2017-02-06 21:54:04 +0800

    Add files via upload

---
+++ hello_prj/testImport.py
@@ -0,0 +1,5 @@
+import sys
+from world.testShell import a
+
+a = 5
+print(a, end = '號說你好')
---
+++ hello_prj/world/testShell.py
@@ -0,0 +1,5 @@
+a = int(input("plz input a number: "))
+if a != 5:
+    print("yes")
+else :
+    print("no")
