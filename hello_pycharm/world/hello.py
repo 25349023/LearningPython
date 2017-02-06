50dd9abf8e1f1e6e8ec03c73b4bb9af3d068bc7c
25349023 <25349023.qq@gmail.com>
2017-02-06 21:57:49 +0800

    Add files via upload

---
+++ hello_pycharm/world/hello.py
@@ -0,0 +1,6 @@
+# a = int(input("plz input a number: "))
+import sys
+
+name = 'world'
+print('hell0: {n1:#^{n2}d} /end,  {str1}'.format(n1=10, n2=5, str1=sys.platform))
+print('len of "你好" = {}'.format(len('你好')))
